915deddacf25e5b1fd3946f630343ff75165a514
AliRaza-Dev678 <151050163+AliRaza-Dev678@users.noreply.github.com>
2023-11-23 10:53:04 +0500

    Add files via upload
    this is exercise 1 of my assignment

---
+++ python projectds.py
@@ -0,0 +1,10 @@
+def calresult(n1,n2):
+    product=n1*n2
+    if product<=1000:
+        return product
+    else:
+        return n1+n2
+num1 =60  
+num2 =20 
+result = calresult(num1,num2) 
+print(result) 
